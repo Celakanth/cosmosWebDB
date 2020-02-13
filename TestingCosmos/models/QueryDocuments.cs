4366bbdf872a35014b6932b8f247909f432b0fcb
Celakanth-VSI <christina.hoare@uprisevsi.co.uk>
2020-02-13 10:23:16 +0000

    first commit

---
+++ TestingCosmos/models/QueryDocuments.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestingCosmos.models
+{
+    public class QueryDocuments
+    {
+
+    }
+
+    
+}
